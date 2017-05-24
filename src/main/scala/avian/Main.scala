ee09b8287283d0fb8300adfa4573c0eced3872bb
La Monade <iomonad@riseup.net>
2017-05-24 13:04:14 +0200

    Moving to Actor model.

--- src/main/scala/avian/Main.scala
+++ src/main/scala/avian/Main.scala
@@ -1,8 +1,10 @@
 package com.avian
 
-import com.avian.worker._
+import com.avian.worker.MainWorker
+import akka.actor._
 
 package object Main extends App {
-
-    Worker("https://gentoo.org")
+    val system:ActorSystem = ActorSystem("WorkerSystem")
+    val w1:ActorRef = system.actorOf(Props[MainWorker], "w1")
+    w1 ! "https://gentoo.org"
 }
--- src/main/scala/avian/Worker.scala
+++ src/main/scala/avian/Worker.scala
@@ -4,15 +4,18 @@ import com.avian.network._
 import com.avian.scrape._
 import com.avian.types._
 import com.avian.database._
+import akka.actor._
 
 /* @Desc: Small sample to show worker process.
  *  TODO: Implement actor class.
  */
-object Worker {
-    def apply(url: String): Unit = {
-        val a = new Client(url)
+class MainWorker(url: String) extends Actor {
+    override def receive = {
+        case s: String => {
+            val a = new Client(s)
 
-        val result = Types.makeIndex(url, a.makeRequest ,Scrape(a.getBody), a.localnode)
-        Database.insertIndex(result)
+            val result = Types.makeIndex(s, a.makeRequest ,Scrape(a.getBody), a.localnode)
+            Database.insertIndex(result)
+        }        
     }
 }
